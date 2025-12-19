89d14a578e5e5e516bfb73fa9935d45e6321e359
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-19 19:51:11 +0530

    Update functionality created

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cmd = args[0];
 const task = args[1];
 const idNum = args[2];
 const description = args[3];
+
 const ID = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 const now = new Date();
 
@@ -23,7 +24,7 @@ let newTask = {
 let addTask = [];
 
 if (cmd == 'add') {
-    
+
     if (fs.existsSync("./task.json")) {
         // read existing data
         let existingData = fs.readFileSync("./task.json", "utf-8");
@@ -41,17 +42,36 @@ if (cmd == 'add') {
         fs.writeFileSync("./task.json", JSON.stringify(addTask, null, 2));
         console.log(`Task added sucessfully, ID: ${ID}`);
     }, 1500)
-    
+
 }
 
-if (cmd == 'list'){
-    try{
+if (cmd == 'list') {
+    try {
         let tasks = fs.readFileSync("./task.json", "utf-8");
         // addTask = []
         addTask = JSON.parse(tasks);
         // addtask = [{Some task}]
         console.log(addTask);
-    }catch(err){
+    } catch (err) {
         console.log("Task list is empty");
     }
+}
+
+if (cmd == "update") {
+    try {
+        let tasks = fs.readFileSync("./task.json", "utf-8");
+        addTask = JSON.parse(tasks);
+   
+        for (let i = 0; i < addTask.length; i++) {
+            if (addTask[i].ID == idNum) {
+                addTask[i].name = newTask.name;
+            }
+        }
+        fs.writeFileSync("./task.json", JSON.stringify(addTask));
+        console.log("Task upadted sucessfully");
+
+    }catch(err){
+        console.log("Task not update", err);
+    }
+    
 }
